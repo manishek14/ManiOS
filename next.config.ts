dbfb2912fd9875a2ab28f4f96b81c554e604a18e
mani shekofteh <96682900+manishek14@users.noreply.github.com>
2026-07-21 11:07:01 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
+  output: "standalone",  
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,  
   },
-  reactStrictMode: false,
+  reactStrictMode: true,  
+  
 };
 
 export default nextConfig;
